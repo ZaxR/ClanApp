fa302ef60bb38ae8a3a1592d8ad9c51746e40f36
Zax <zax.rosenberg@gmail.com>
2017-06-08 15:29:47 -0500

    Added port variable

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 from app import app
+import os
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=4000)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
